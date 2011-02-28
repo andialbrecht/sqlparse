2bd7658034545aa7db5752cd35d400526af5439e
Andi Albrecht <albrecht.andi@gmail.com>
2011-02-28 21:23:41 +0100

    Handle REPLACE as DML token and recognize hex numbers (thanks Ervin).

--- sqlparse/lexer.py
+++ sqlparse/lexer.py
@@ -177,6 +177,7 @@ class Lexer:
             (r'@[a-zA-Z_][a-zA-Z0-9_]+', tokens.Name),
             (r'[<>=~!]+', tokens.Operator.Comparison),
             (r'[+/@#%^&|`?^-]+', tokens.Operator),
+            (r'0x[0-9a-fA-F]+', tokens.Number.Hexadecimal),
             (r'[0-9]*\.[0-9]+', tokens.Number.Float),
             (r'[0-9]+', tokens.Number.Integer),
             # TODO: Backslash escapes?
