ea2b23a46b4dacf1d76cdbdaa172e25e3b733466
Andi Albrecht <albrecht.andi@gmail.com>
2011-03-17 09:48:43 +0100

    Move regression tests.

--- tests/test_regressions.py
+++ tests/test_regressions.py
@@ -3,12 +3,31 @@
 import unittest
 
 import sqlparse
-from sqlparse import tokens
+from sqlparse import sql
+from sqlparse import tokens as T
 
 
 class RegressionTests(unittest.TestCase):
 
+    def test_issue9(self):
+        # make sure where doesn't consume parenthesis
+        p = sqlparse.parse('(where 1)')[0]
+        self.assert_(isinstance(p, sql.Statement))
+        self.assertEqual(len(p.tokens), 1)
+        self.assert_(isinstance(p.tokens[0], sql.Parenthesis))
+        prt = p.tokens[0]
+        self.assertEqual(len(prt.tokens), 3)
+        self.assertEqual(prt.tokens[0].ttype, T.Punctuation)
+        self.assertEqual(prt.tokens[-1].ttype, T.Punctuation)
+
+    def test_issue13(self):
+        parsed = sqlparse.parse(("select 'one';\n"
+                                 "select 'two\\'';\n"
+                                 "select 'three';"))
+        self.assertEqual(len(parsed), 3)
+        self.assertEqual(str(parsed[1]).strip(), "select 'two\\'';")
+
     def test_issue34(self):
         t = sqlparse.parse("create")[0].token_first()
-        self.assertEqual(t.match(tokens.Keyword.DDL, "create"), True)
-        self.assertEqual(t.match(tokens.Keyword.DDL, "CREATE"), True)
+        self.assertEqual(t.match(T.Keyword.DDL, "create"), True)
+        self.assertEqual(t.match(T.Keyword.DDL, "CREATE"), True)
