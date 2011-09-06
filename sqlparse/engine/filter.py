bcaf3269b3a8746a4ee8c848256d5f2b11b25d27
Andi Albrecht <albrecht.andi@gmail.com>
2011-09-06 08:01:12 +0200

    Code cleanup.

--- sqlparse/engine/filter.py
+++ sqlparse/engine/filter.py
@@ -61,14 +61,15 @@ class StatementFilter(TokenFilter):
         if unified == 'END':
             # Should this respect a preceeding BEGIN?
             # In CASE ... WHEN ... END this results in a split level -1.
-            self._begin_depth = max(0, self._begin_depth-1)
+            self._begin_depth = max(0, self._begin_depth - 1)
             return -1
 
         if ttype is T.Keyword.DDL and unified.startswith('CREATE'):
             self._is_create = True
             return 0
 
-        if unified in ('IF', 'FOR') and self._is_create and self._begin_depth > 0:
+        if (unified in ('IF', 'FOR')
+            and self._is_create and self._begin_depth > 0):
             return 1
 
         # Default
