73465819c9d5105f57a9cf5579c5dc78964b37e4
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-05-17 23:11:05 +0200

    Added functions from AntiORM

---
+++ sqlparse/functions.py
@@ -0,0 +1,44 @@
+'''
+Created on 17/05/2012
+
+@author: piranna
+
+Several utility functions to extract info from the SQL sentences
+'''
+
+from sqlparse.filters  import ColumnsSelect, Limit
+from sqlparse.pipeline import Pipeline
+from sqlparse.tokens   import Keyword, Whitespace
+
+
+def GetLimit(stream):
+    """Function that return the LIMIT of a input SQL """
+    pipe = Pipeline()
+
+    pipe.append(Limit())
+
+    result = pipe(stream)
+    try:
+        return int(result)
+    except ValueError:
+        return result
+
+
+def GetColumns(stream):
+    """Function that return the colums of a SELECT query"""
+    pipe = Pipeline()
+
+    pipe.append(ColumnsSelect())
+
+    return pipe(stream)
+
+
+class IsType():
+    """Functor that return is the statement is of a specific type"""
+    def __init__(self, type):
+        self.type = type
+
+    def __call__(self, stream):
+        for token_type, value in stream:
+            if token_type in Whitespace: continue
+            return token_type in Keyword and value == self.type
