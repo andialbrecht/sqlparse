c9dc9c8b79a3e290c28761a786993b02eff705d6
Darik Gamble <darik.gamble@gmail.com>
2015-03-04 10:34:05 -0500

    get rid of tokens.Punctuation.ArrayIndex, add negative lookbehind for sqlite identifiers

--- sqlparse/tokens.py
+++ sqlparse/tokens.py
@@ -57,7 +57,6 @@ Literal = Token.Literal
 String = Literal.String
 Number = Literal.Number
 Punctuation = Token.Punctuation
-ArrayIndex = Punctuation.ArrayIndex
 Operator = Token.Operator
 Comparison = Operator.Comparison
 Wildcard = Token.Wildcard
