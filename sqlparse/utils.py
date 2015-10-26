92d12ec8c6d62e952d5a8d194db508a24c429ac9
Andi Albrecht <albrecht.andi@gmail.com>
2015-10-26 20:22:46 +0100

    Remove conditional for OrderedDict.
    All supported Python versions have OrderedDict.

--- sqlparse/utils.py
+++ sqlparse/utils.py
@@ -5,61 +5,39 @@ Created on 17/05/2012
 '''
 
 import re
-
-try:
-    from collections import OrderedDict
-except ImportError:
-    OrderedDict = None
+from collections import OrderedDict
 
 
-if OrderedDict:
-    class Cache(OrderedDict):
-        """Cache with LRU algorithm using an OrderedDict as basis
-        """
-        def __init__(self, maxsize=100):
-            OrderedDict.__init__(self)
+class Cache(OrderedDict):
+    """Cache with LRU algorithm using an OrderedDict as basis
+    """
+    def __init__(self, maxsize=100):
+        OrderedDict.__init__(self)
 
-            self._maxsize = maxsize
+        self._maxsize = maxsize
 
-        def __getitem__(self, key, *args, **kwargs):
-            # Get the key and remove it from the cache, or raise KeyError
-            value = OrderedDict.__getitem__(self, key)
+    def __getitem__(self, key, *args, **kwargs):
+        # Get the key and remove it from the cache, or raise KeyError
+        value = OrderedDict.__getitem__(self, key)
+        del self[key]
+
+        # Insert the (key, value) pair on the front of the cache
+        OrderedDict.__setitem__(self, key, value)
+
+        # Return the value from the cache
+        return value
+
+    def __setitem__(self, key, value, *args, **kwargs):
+        # Key was inserted before, remove it so we put it at front later
+        if key in self:
             del self[key]
 
-            # Insert the (key, value) pair on the front of the cache
-            OrderedDict.__setitem__(self, key, value)
+        # Too much items on the cache, remove the least recent used
+        elif len(self) >= self._maxsize:
+            self.popitem(False)
 
-            # Return the value from the cache
-            return value
-
-        def __setitem__(self, key, value, *args, **kwargs):
-            # Key was inserted before, remove it so we put it at front later
-            if key in self:
-                del self[key]
-
-            # Too much items on the cache, remove the least recent used
-            elif len(self) >= self._maxsize:
-                self.popitem(False)
-
-            # Insert the (key, value) pair on the front of the cache
-            OrderedDict.__setitem__(self, key, value, *args, **kwargs)
-
-else:
-    class Cache(dict):
-        """Cache that reset when gets full
-        """
-        def __init__(self, maxsize=100):
-            dict.__init__(self)
-
-            self._maxsize = maxsize
-
-        def __setitem__(self, key, value, *args, **kwargs):
-            # Reset the cache if we have too much cached entries and start over
-            if len(self) >= self._maxsize:
-                self.clear()
-
-            # Insert the (key, value) pair on the front of the cache
-            dict.__setitem__(self, key, value, *args, **kwargs)
+        # Insert the (key, value) pair on the front of the cache
+        OrderedDict.__setitem__(self, key, value, *args, **kwargs)
 
 
 def memoize_generator(func):
