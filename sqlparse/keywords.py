65a21f54851233b8747b1b28c6ecfcf27233684b
Victor Uriarte <victor.m.uriarte@intel.com>
2016-06-28 14:28:07 -0700

    Format keywords.py

--- sqlparse/keywords.py
+++ sqlparse/keywords.py
@@ -12,7 +12,9 @@ from sqlparse import tokens
 
 def is_keyword(value):
     val = value.upper()
-    return (KEYWORDS_COMMON.get(val) or KEYWORDS_ORACLE.get(val) or KEYWORDS.get(val, tokens.Name)), value
+    return (KEYWORDS_COMMON.get(val) or
+            KEYWORDS_ORACLE.get(val) or
+            KEYWORDS.get(val, tokens.Name)), value
 
 
 SQL_REGEX = {
