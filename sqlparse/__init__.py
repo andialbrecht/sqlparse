c2561491b8adb10ad30932d4d61781a97edad675
Andi Albrecht <albrecht.andi@gmail.com>
2016-08-13 17:42:11 +0200

    This is 0.2.1.

--- sqlparse/__init__.py
+++ sqlparse/__init__.py
@@ -17,7 +17,7 @@ from sqlparse import formatter
 
 from sqlparse.compat import text_type
 
-__version__ = '0.2.1.dev0'
+__version__ = '0.2.1'
 __all__ = ['engine', 'filters', 'formatter', 'sql', 'tokens', 'cli']
 
 
