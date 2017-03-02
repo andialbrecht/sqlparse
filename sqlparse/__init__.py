1b9e371ee55a60ecb41dafa4945898424c095dff
Andi Albrecht <andi@lap-pu35.prounix.local>
2017-03-02 05:41:20 +0100

    Bump version.

--- sqlparse/__init__.py
+++ sqlparse/__init__.py
@@ -17,7 +17,7 @@ from sqlparse import formatter
 
 from sqlparse.compat import text_type
 
-__version__ = '0.2.3.dev0'
+__version__ = '0.2.3'
 __all__ = ['engine', 'filters', 'formatter', 'sql', 'tokens', 'cli']
 
 
