3853a076c63493f43e969cf4ddd2e642e92f02fb
Andi Albrecht <albrecht.andi@gmail.com>
2018-07-31 08:12:54 +0200

    Update copyright header (fixes #372).

--- sqlparse/filters/right_margin.py
+++ sqlparse/filters/right_margin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2016 Andi Albrecht, albrecht.andi@gmail.com
+# Copyright (C) 2009-2018 the sqlparse authors and contributors
+# <see AUTHORS file>
 #
 # This module is part of python-sqlparse and is released under
 # the BSD License: https://opensource.org/licenses/BSD-3-Clause
