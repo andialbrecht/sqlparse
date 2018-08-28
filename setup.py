488505f6c448e7eb0e4a1915bdc5b6130d44a68a
Jon Dufresne <jon.dufresne@gmail.com>
2018-08-28 02:47:23 -0700

    Add trove classifier for Python 3.7 support
    Testing was added in b9ff0bd225e71f4fe522859b49b8194a725b4421.

--- setup.py
+++ setup.py
@@ -97,6 +97,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Database',
         'Topic :: Software Development',
     ],
