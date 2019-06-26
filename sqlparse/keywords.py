5a3164d8db9ce5bbd64480bcb63eb1ac7a587151
Andi Albrecht <albrecht.andi@gmail.com>
2019-06-26 12:35:02 +0200

    Optimize parsing of literal blocks between dollar signs (fixes #491).

--- sqlparse/keywords.py
+++ sqlparse/keywords.py
@@ -38,7 +38,7 @@ SQL_REGEX = {
 
         (r"`(``|[^`])*`", tokens.Name),
         (r"´(´´|[^´])*´", tokens.Name),
-        (r'(\$(?:[_A-ZÀ-Ü]\w*)?\$)[\s\S]*?\1', tokens.Literal),
+        (r'((?<!\S)\$(?:[_A-ZÀ-Ü]\w*)?\$)[\s\S]*?\1', tokens.Literal),
 
         (r'\?', tokens.Name.Placeholder),
         (r'%(\(\w+\))?s', tokens.Name.Placeholder),
