0e187cd55bb6bd189206a72abbe94667da9437ce
Daniel Harding <dharding@living180.net>
2021-05-12 14:56:38 +0300

    Make TokenList.value a property not an attribute.
    The fact that a new value was being computed each time
    TokenList.group_tokens() is called caused supra-linear runtime when
    token grouping was enabled.
    
    Address by making TokenList.value a dynamically-computed property rather
    than a static attribute.

--- sqlparse/sql.py
+++ sqlparse/sql.py
@@ -45,6 +45,9 @@ class TokenBase:
     def __init__(self):
         self.parent = None
 
+    def __str__(self):
+        return self.value
+
     # Pending tokenlist __len__ bug fix
     # def __len__(self):
     #     return len(self.value)
@@ -144,9 +147,6 @@ class Token(TokenBase):
         self.is_whitespace = ttype in T.Whitespace
         self.normalized = value.upper() if self.is_keyword else value
 
-    def __str__(self):
-        return self.value
-
     def _get_repr_name(self):
         return str(self.ttype).split('.')[-1]
 
@@ -158,12 +158,11 @@ class Token(TokenBase):
 class TokenList(TokenBase):
     """A group of tokens.
 
-    It has two instance attributes:
-    ``value`` is the unchanged value of the token and ``tokens`` is a list of
+    It has one instance attribute ``tokens`` which holds a list of
     child-tokens.
     """
 
-    __slots__ = ('tokens', 'value')
+    __slots__ = 'tokens'
 
     is_group = True
     ttype = None
@@ -173,10 +172,10 @@ class TokenList(TokenBase):
     def __init__(self, tokens=None):
         super().__init__()
         self.tokens = tokens or []
-        self.value = str(self)
         [setattr(token, 'parent', self) for token in self.tokens]
 
-    def __str__(self):
+    @property
+    def value(self):
         return ''.join(token.value for token in self.flatten())
 
     @property
@@ -341,7 +340,6 @@ class TokenList(TokenBase):
             grp = start
             grp.tokens.extend(subtokens)
             del self.tokens[start_idx + 1:end_idx]
-            grp.value = str(start)
         else:
             subtokens = self.tokens[start_idx:end_idx]
             grp = grp_cls(subtokens)
