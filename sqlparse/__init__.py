377dfa084cd5c38cb3a97460bc86a80b1135a0cc
Andi Albrecht <albrecht.andi@gmail.com>
2021-09-10 08:55:28 +0200

    Switch back to development mode.

--- sqlparse/__init__.py
+++ sqlparse/__init__.py
@@ -16,7 +16,7 @@ from sqlparse import filters
 from sqlparse import formatter
 
 
-__version__ = '0.4.2'
+__version__ = '0.4.3.dev0'
 __all__ = ['engine', 'filters', 'formatter', 'sql', 'tokens', 'cli']
 
 
