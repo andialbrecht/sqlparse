fba15d34a01739c10596d727e20b89762298eea8
Andi Albrecht <albrecht.andi@gmail.com>
2022-09-23 20:31:10 +0200

    Bump version.

--- sqlparse/__init__.py
+++ sqlparse/__init__.py
@@ -16,7 +16,7 @@ from sqlparse import filters
 from sqlparse import formatter
 
 
-__version__ = '0.4.3.dev0'
+__version__ = '0.4.3'
 __all__ = ['engine', 'filters', 'formatter', 'sql', 'tokens', 'cli']
 
 
