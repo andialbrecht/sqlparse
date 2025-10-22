92511b110b0c53da67a0a4694e919bd6e18d6051
Stefan Warnat <ich@stefanwarnat.de>
2025-10-22 22:43:53 +0200

    Add 'INTERSECT' to Close Keywords in Where clause

--- sqlparse/sql.py
+++ sqlparse/sql.py
@@ -550,7 +550,7 @@ class Where(TokenList):
     """A WHERE clause."""
     M_OPEN = T.Keyword, 'WHERE'
     M_CLOSE = T.Keyword, (
-        'ORDER BY', 'GROUP BY', 'LIMIT', 'UNION', 'UNION ALL', 'EXCEPT',
+        'ORDER BY', 'GROUP BY', 'LIMIT', 'UNION', 'UNION ALL', 'EXCEPT', 'INTERSECT',
         'HAVING', 'RETURNING', 'INTO')
 
 
